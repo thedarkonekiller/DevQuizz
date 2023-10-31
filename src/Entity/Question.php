9bd105c12a7d44e98eb6910f50fd0d4fa0deca93
Orrorine <tojmsa@gmail.com>
2023-10-31 16:51:12 +0100

    Ajout de l'entite Questions associee a Quizz sous forme de relation manyToOne

---
+++ src/Entity/Question.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuestionRepository::class)]
+class Question
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $titled = null;
+
+    #[ORM\ManyToOne(inversedBy: 'questions')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Quizz $quizz = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitled(): ?string
+    {
+        return $this->titled;
+    }
+
+    public function setTitled(string $titled): static
+    {
+        $this->titled = $titled;
+
+        return $this;
+    }
+
+    public function getQuizz(): ?Quizz
+    {
+        return $this->quizz;
+    }
+
+    public function setQuizz(?Quizz $quizz): static
+    {
+        $this->quizz = $quizz;
+
+        return $this;
+    }
+}
