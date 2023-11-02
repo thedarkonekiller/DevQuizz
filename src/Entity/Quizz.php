f18077ccaae74604c542e7cd221b89af0c6a145e
Orrorine <tojmsa@gmail.com>
2023-11-02 13:03:14 +0100

    Ajout QuestionCrudController

--- src/Entity/Quizz.php
+++ src/Entity/Quizz.php
@@ -54,6 +54,12 @@ class Quizz
         return $this->id;
     }
 
+/*     public function setId(int $id): static
+    {
+        $this->id = $id;
+        return $this;
+    } */
+
     public function getName(): ?string
     {
         return $this->name;
